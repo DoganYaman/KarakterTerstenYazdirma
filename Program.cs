47794699d556dc88a81453874d600d90efdc81e9
Dogan Yaman <yamandogan963@gmail.com>
2024-01-12 23:16:47 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace karakter_tersten_yazdirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Tersten yazdırılacak ifadeyi giriniz : ");
+            string ifade = Console.ReadLine();
+            TerstenYazdir(ifade);
+            
+        }
+
+        static void TerstenYazdir(string ifade)
+        {
+            string[] stringDizi = ifade.Split(' ');
+
+            for (int i = 0; i < stringDizi.Length; i++)
+            {
+                char[] charDizi = stringDizi[i].ToCharArray();
+                Array.Reverse(charDizi);
+                string kelime = new string(charDizi);
+                Console.Write(kelime + " ");
+            }
+
+        }
+    }
+}
